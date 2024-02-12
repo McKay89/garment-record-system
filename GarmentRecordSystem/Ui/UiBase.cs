9c3d50121c4b54a3b761cce3705bff527afd72ea
McKay <kg89szeged@gmail.com>
2024-02-12 21:53:02 +0100

    [ ADD ] UiBase class created

---
+++ GarmentRecordSystem/Ui/UiBase.cs
@@ -0,0 +1,27 @@
+﻿using GarmentRecordSystem.Service.Logger;
+
+namespace GarmentRecordSystem.Ui;
+
+public abstract class UiBase
+{
+    private readonly string _title;
+    private readonly ILogger _logger;
+
+    protected UiBase(string title)
+    {
+        _title = title;
+        _logger = new ConsoleLogger();
+    }
+    
+    public void DisplayTitle()
+    {
+        _logger.LogMenuTitle(_title, "TITLE");
+    }
+
+    public void PrintSubmenuInfos()
+    {
+        Console.WriteLine(" Type 'back' if you want to return to Main Menu\n");
+    }
+    
+    public abstract void Run();
+}
