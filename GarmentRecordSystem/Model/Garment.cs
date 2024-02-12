3eef346c422adaea8ba05039aec386a11f0c1efb
McKay <kg89szeged@gmail.com>
2024-02-12 14:42:21 +0100

    [ ADD ] garment record created

---
+++ GarmentRecordSystem/Model/Garment.cs
@@ -0,0 +1,5 @@
+﻿using GarmentRecordSystem.Enums;
+
+namespace GarmentRecordSystem.Model;
+
+public record Garment(uint GarmentId, string BrandName, DateOnly PurchaseDate, string Color, Sizes Size);
