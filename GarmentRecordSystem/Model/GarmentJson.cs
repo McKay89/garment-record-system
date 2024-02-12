89428d5fcec29aaf9026059af7065a5367950177
McKay <kg89szeged@gmail.com>
2024-02-12 16:42:21 +0100

    [ ADD ] garment class for the json reader

---
+++ GarmentRecordSystem/Model/GarmentJson.cs
@@ -0,0 +1,10 @@
+﻿namespace GarmentRecordSystem.Model;
+
+public class GarmentJson
+{
+    public uint GarmentId { get; set; }
+    public string BrandName { get; set; }
+    public string PurchaseDate { get; set; }
+    public string Color { get; set; }
+    public string Size { get; set; }
+}
