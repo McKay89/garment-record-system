882eec3be114f9d2cb6c2271e3d414df57ff4e69
McKay <kg89szeged@gmail.com>
2024-02-12 14:41:46 +0100

    [ ADD ] console logger & logger interface created

---
+++ GarmentRecordSystem/Service/Logger/ConsoleLogger.cs
@@ -0,0 +1,12 @@
+﻿namespace GarmentRecordSystem.Service.Logger;
+
+public class ConsoleLogger : ILogger
+{
+    private static string CreateLogEntry(string message, string type) => $"[{DateTime.Now}] {type}: {message}";
+
+    public void LogInfo(string message, string type)
+    {
+        var entry = CreateLogEntry(message, type);
+        Console.WriteLine(entry);
+    }
+}
---
+++ GarmentRecordSystem/Service/Logger/ILogger.cs
@@ -0,0 +1,6 @@
+﻿namespace GarmentRecordSystem.Service.Logger;
+
+public interface ILogger
+{
+    public void LogInfo(string message, string type);
+}
