e54fe042a2ed94ab84042e306e9abf125de61947
McKay <kg89szeged@gmail.com>
2024-02-12 21:51:12 +0100

    [ ADD ] garment browser class created

---
+++ GarmentRecordSystem/Service/Products/Browser/GarmentBrowser.cs
@@ -0,0 +1,52 @@
+﻿using GarmentRecordSystem.Model;
+
+namespace GarmentRecordSystem.Service.Products.Browser;
+
+public class GarmentBrowser : IGarmentBrowser
+{
+    private IEnumerable<GarmentJson>? _garments;
+    public GarmentBrowser(IReadOnlyCollection<GarmentJson>? garments)
+    {
+        if (garments != null) _garments = garments;
+    }
+    
+    public IEnumerable<Garment> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Garment> SortByBrandName()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Garment> SortByPurchaseDate()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Garment> SortBySize()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Garment> SortByColor()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool SetNewItem(object item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UpdateItem(uint id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteItem(uint id)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ GarmentRecordSystem/Service/Products/Browser/IGarmentBrowser.cs
@@ -0,0 +1,17 @@
+﻿using GarmentRecordSystem.Model;
+
+namespace GarmentRecordSystem.Service.Products.Browser;
+
+public interface IGarmentBrowser
+{
+    IEnumerable<Garment> GetAll();
+    
+    IEnumerable<Garment> SortByBrandName();
+    IEnumerable<Garment> SortByPurchaseDate();
+    IEnumerable<Garment> SortBySize();
+    IEnumerable<Garment> SortByColor();
+    
+    bool SetNewItem(object item);
+    bool UpdateItem(uint id);
+    bool DeleteItem(uint id);
+}
