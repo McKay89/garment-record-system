5e6ffa8dc6933cdcf1aff24842993457834ccd25
McKay <kg89szeged@gmail.com>
2024-02-13 00:39:15 +0100

    [ UPDATE ] constructor created with garmentBrowser

--- GarmentRecordSystem/Ui/Factory/AddGarmentUiFactory.cs
+++ GarmentRecordSystem/Ui/Factory/AddGarmentUiFactory.cs
@@ -1,11 +1,20 @@
-﻿namespace GarmentRecordSystem.Ui.Factory;
+﻿using GarmentRecordSystem.Service.Products.Browser;
+
+namespace GarmentRecordSystem.Ui.Factory;
 
 public class AddGarmentUiFactory : UiFactoryBase
 {
+    private readonly IGarmentBrowser _garmentBrowser;
+    
+    public AddGarmentUiFactory(IGarmentBrowser garmentBrowser)
+    {
+        _garmentBrowser = garmentBrowser;
+    }
+
     public override string UiName => "Add";
     
     public override UiBase Create()
     {
-        return new AddGarmentUi(UiName);
+        return new AddGarmentUi(UiName, _garmentBrowser);
     }
 }
