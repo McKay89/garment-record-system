c510c52e6acb6c9b37870d332392189c8fa3e83a
McKay <kg89szeged@gmail.com>
2024-02-12 21:55:03 +0100

    [ ADD ] AddGarmentUiFactory class created

---
+++ GarmentRecordSystem/Ui/Factory/AddGarmentUiFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace GarmentRecordSystem.Ui.Factory;
+
+public class AddGarmentUiFactory : UiFactoryBase
+{
+    public override string UiName => "Add";
+    
+    public override UiBase Create()
+    {
+        return new AddGarmentUi(UiName);
+    }
+}
