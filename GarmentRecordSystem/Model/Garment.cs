33b71c7a4bf828508cc5659c2610515bac836888
McKay <kg89szeged@gmail.com>
2024-02-13 00:38:29 +0100

    [ UPDATE ] all fields setter was modified to init

--- GarmentRecordSystem/Model/Garment.cs
+++ GarmentRecordSystem/Model/Garment.cs
@@ -4,9 +4,9 @@ namespace GarmentRecordSystem.Model;
 
 public class Garment
 {
-    public uint GarmentId { get; set; }
-    public string BrandName { get; set; }
-    public DateOnly PurchaseDate { get; set; }
-    public string Color { get; set; }
-    public Sizes Size { get; set; }
+    public int GarmentId { get; init; }
+    public string BrandName { get; init; }
+    public DateOnly PurchaseDate { get; init; }
+    public string Color { get; init; }
+    public Sizes Size { get; init; }
 }
