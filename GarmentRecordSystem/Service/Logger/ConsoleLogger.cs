76ebc0e4c4a23c926210844ab2963347d3a7c63c
McKay <kg89szeged@gmail.com>
2024-02-12 21:52:23 +0100

    [ UPDATE ] logger updated with colors and LogMenu, LogMenuTitle methods

--- GarmentRecordSystem/Service/Logger/ConsoleLogger.cs
+++ GarmentRecordSystem/Service/Logger/ConsoleLogger.cs
@@ -2,11 +2,53 @@
 
 public class ConsoleLogger : ILogger
 {
-    private static string CreateLogEntry(string message, string type) => $"[{DateTime.Now}] {type}: {message}";
+    private static string CreateLogEntry(string message, string type) => $"\n [{type}] {message}";
+    private static string CreateMenuEntry(int num, string title) => $"   [{num}] - {title}";
+    private static string CreateMenuTitleEntry(string text) => $"\n ------ [ {text} ] ------";
 
-    public void LogInfo(string message, string type)
+    public void LogMessage(string message, string type)
     {
         var entry = CreateLogEntry(message, type);
+        ChangeLogColor(type);
+        
         Console.WriteLine(entry);
+        Console.ResetColor();
+    }
+    
+    public void LogMenu(int num, string title)
+    {
+        var entry = CreateMenuEntry(num, title);
+        Console.WriteLine(entry);
+    }
+
+    public void LogMenuTitle(string text, string type)
+    {
+        var entry = CreateMenuTitleEntry(text);
+        ChangeLogColor(type);
+        
+        Console.WriteLine(entry);
+        Console.ResetColor();
+    }
+
+    private void ChangeLogColor(string type)
+    {
+        switch (type)
+        {
+            case "INFO":
+                Console.ForegroundColor = ConsoleColor.Green;
+                break;
+            case "ERROR":
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            case "WARNING":
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case "TITLE":
+                Console.ForegroundColor = ConsoleColor.Blue;
+                break;
+            default:
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                break;
+        } 
     }
 }
--- GarmentRecordSystem/Service/Logger/ILogger.cs
+++ GarmentRecordSystem/Service/Logger/ILogger.cs
@@ -2,5 +2,7 @@
 
 public interface ILogger
 {
-    public void LogInfo(string message, string type);
+    public void LogMessage(string message, string type);
+    public void LogMenu(int num, string title);
+    public void LogMenuTitle(string text, string type);
 }
