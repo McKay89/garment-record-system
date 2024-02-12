61639e78ac6fd31206b26e9d97376fe9bcbd9d8d
McKay <kg89szeged@gmail.com>
2024-02-12 16:41:43 +0100

    [ ADD ] Json reader created

---
+++ GarmentRecordSystem/Controllers/JsonFileReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using GarmentRecordSystem.Model;
+
+namespace GarmentRecordSystem.Controllers;
+
+public static class JsonFileReader
+{
+    public static List<GarmentJson>? Read(string filePath)
+    {
+        try
+        {
+            var text = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<GarmentJson>>(text);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine($"File not found: {filePath}");
+            return null;
+        }
+        catch (JsonException)
+        {
+            Console.WriteLine($"Wrong JSON format: {filePath}");
+            return null;
+        }
+    }
+}
