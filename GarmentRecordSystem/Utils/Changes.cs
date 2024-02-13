64e4138f03dd4d94a533e939ca77cc5db72b0812
McKay <kg89szeged@gmail.com>
2024-02-13 18:00:04 +0100

    [ ADD ] any changes will be saved into a counter

---
+++ GarmentRecordSystem/Utils/Changes.cs
@@ -0,0 +1,30 @@
+﻿using System.Dynamic;
+
+namespace GarmentRecordSystem.Utils;
+
+public static class Changes
+{
+    private static int AddCounter { get; set; } = 0;
+    private static int UpdateCounter { get; set; } = 0;
+    private static int DeleteCounter { get; set; } = 0;
+    private static int SortCounter { get; set; } = 0;
+
+    public static void IncrementAddCounter() => AddCounter++;
+    public static void IncrementUpdateCounter() => UpdateCounter++;
+    public static void IncrementDeleteCounter() => DeleteCounter++;
+    public static void IncrementSortCounter() => SortCounter++;
+
+    public static Tuple<int,int,int,int> GetChanges()
+    {
+        var changes = Tuple.Create(AddCounter, UpdateCounter, DeleteCounter, SortCounter);
+        return changes;
+    }
+    
+    public static void ResetCounters()
+    {
+        AddCounter = 0;
+        UpdateCounter = 0;
+        DeleteCounter = 0;
+        SortCounter = 0;
+    }
+}
