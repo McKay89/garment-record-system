5dac29913cbbcd0732b58fc80be993b9e10105d6
McKay <kg89szeged@gmail.com>
2024-02-13 21:11:58 +0100

    [ ADD ] exit UI factory created

---
+++ GarmentRecordSystem/Ui/ExitGarmentUi.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+using GarmentRecordSystem.Enums;
+using GarmentRecordSystem.Model;
+using GarmentRecordSystem.Service.Logger;
+using GarmentRecordSystem.Service.Products.Browser;
+using GarmentRecordSystem.Ui.Factory;
+using GarmentRecordSystem.Utils;
+
+namespace GarmentRecordSystem.Ui;
+
+public class ExitGarmentUi : UiBase
+{
+    private readonly ILogger _logger = new ConsoleLogger();
+    
+    public ExitGarmentUi(string title) : base(title)
+    {
+
+    }
+
+    public override void Run()
+    {
+        // Exit application
+        _logger.LogMessage("Application is shutting down in 2 sec...", "INFO");
+        Thread.Sleep(2000);
+        Environment.Exit(0);
+    }
+}
---
+++ GarmentRecordSystem/Ui/Factory/ExitGarmentUiFactory.cs
@@ -0,0 +1,18 @@
+﻿using GarmentRecordSystem.Service.Products.Browser;
+
+namespace GarmentRecordSystem.Ui.Factory;
+
+public class ExitGarmentUiFactory : UiFactoryBase
+{
+    public ExitGarmentUiFactory()
+    {
+        
+    }
+
+    public override string UiName => "Exit";
+    
+    public override UiBase Create(SortedList<int, UiFactoryBase> factories)
+    {
+        return new ExitGarmentUi(UiName);
+    }
+}
