b0cfedfdf4063245d092c6c79d70115e08e7b973
McKay <kg89szeged@gmail.com>
2024-02-13 16:56:55 +0100

    [ UPDATE ] refactored and much cleaner

--- GarmentRecordSystem/Program.cs
+++ GarmentRecordSystem/Program.cs
@@ -2,9 +2,8 @@
 using GarmentRecordSystem.Ui;
 using GarmentRecordSystem.Controllers;
 using GarmentRecordSystem.Model;
+using GarmentRecordSystem.Service;
 using GarmentRecordSystem.Service.Products.Browser;
-using GarmentRecordSystem.Ui.Factory;
-using GarmentRecordSystem.Ui.Selector;
 
 // Create logger
 ILogger logger = new ConsoleLogger();
@@ -12,24 +11,14 @@ ILogger logger = new ConsoleLogger();
 // Read JSON file (every garment is an object)
 var garmentsJson = JsonFileReader.Read(@"./garments.json");
 
-
-IGarmentBrowser garmentBrowser = garmentsJson is { Count: 0 }
-    ? new GarmentBrowser(null, logger)
+// Create Garment Browser instance
+IGarmentBrowser garmentBrowser = garmentsJson is { Count: 0 } or null
+    ? new GarmentBrowser(new List<GarmentJson>(), logger)
     : new GarmentBrowser(garmentsJson, logger);
 
-// Create UI Factory Bases
-UiFactoryBase addGarmentUiFactory = new AddGarmentUiFactory(garmentBrowser);
+// Create UI Factories
+UiFactory uiFactory = new UiFactory(garmentBrowser);
+var uiFactories = UiFactory.GetFactories();
 
-// Create factory list
-SortedList<int, UiFactoryBase> factories = new SortedList<int, UiFactoryBase>
-{
-    { 1, addGarmentUiFactory }      // UI for creating new garment
-};
-
-// Create UI Selector
-var uiSelector = new UiSelector(logger, factories);
-UiBase ui = uiSelector.Select();
-
-ui.DisplayTitle();
-ui.PrintSubmenuInfos();
-ui.Run();
+// Start in Main Menu
+Navigator.MainMenu(logger, uiFactories);
