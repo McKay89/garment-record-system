e4726e722e7a5c5ff972d5531652af58dcc24df4
McKay <kg89szeged@gmail.com>
2024-02-13 20:42:04 +0100

    [ ADD ] save garment UI created and saving logic implemented

---
+++ GarmentRecordSystem/Ui/Factory/SaveGarmentUiFactory.cs
@@ -0,0 +1,25 @@
+﻿using GarmentRecordSystem.Service.Products.Browser;
+using GarmentRecordSystem.Utils;
+
+namespace GarmentRecordSystem.Ui.Factory;
+
+public class SaveGarmentUiFactory : UiFactoryBase
+{
+    private readonly IGarmentBrowser _garmentBrowser;
+    
+    public SaveGarmentUiFactory(IGarmentBrowser garmentBrowser)
+    {
+        _garmentBrowser = garmentBrowser;
+    }
+
+    public override string UiName => $"Save {(Changes.GetChangeSum() == 0 
+        ? "( no changes )" 
+        : $"( {Changes.GetChangeSum()} changes are not saved )")}";
+    
+    private static string UiName2 => "Save";
+    
+    public override UiBase Create(SortedList<int, UiFactoryBase> factories)
+    {
+        return new SaveGarmentUi(UiName2, _garmentBrowser, factories);
+    }
+}
---
+++ GarmentRecordSystem/Ui/SaveGarmentUi.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.InteropServices;
+using GarmentRecordSystem.Controllers;
+using GarmentRecordSystem.Enums;
+using GarmentRecordSystem.Model;
+using GarmentRecordSystem.Service.Logger;
+using GarmentRecordSystem.Service.Products.Browser;
+using GarmentRecordSystem.Ui.Factory;
+using GarmentRecordSystem.Utils;
+
+namespace GarmentRecordSystem.Ui;
+
+public class SaveGarmentUi : UiBase
+{
+    private readonly IGarmentBrowser _garmentBrowser;
+    private readonly ILogger _logger = new ConsoleLogger();
+    private readonly SortedList<int, UiFactoryBase> _factories;
+    
+    public SaveGarmentUi(string title, IGarmentBrowser garmentBrowser, SortedList<int, UiFactoryBase> factories) : base(title)
+    {
+        _garmentBrowser = garmentBrowser;
+        _factories = factories;
+    }
+
+    public override void Run()
+    {
+        var changes = Changes.GetChanges();
+
+        if (Changes.GetChangeSum() == 0)
+        {
+            _logger.LogMessage("There are no changes to save ! Returning to Main Menu...", "INFO");
+            Thread.Sleep(2000);
+            ReturnToMainMenu();
+        }
+        
+        // Log the changes
+        _logger.LogMessage("Saving into the JSON...", "INFO");
+        Console.WriteLine($"  Changes: " + 
+                          $"{(changes.Item1 > 0 ? $"{changes.Item1} creation, " : "")}" +
+                          $"{(changes.Item2 > 0 ? $"{changes.Item2} modification, " : "")}" +
+                          $"{(changes.Item3 > 0 ? $"{changes.Item3} deletion, " : "")}" +
+                          $"{(changes.Item4 > 0 ? $"{changes.Item4} sort " : "")}");
+        
+        // Save Garments into JSON
+        var allGarments = _garmentBrowser.GetAll();
+        bool saveResult = JsonFileWriter.Write(allGarments);
+
+        if (saveResult)
+        {
+            _logger.LogMessage("Data successfully saved into JSON !", "SUCCESS");
+            Changes.ResetCounters();
+            WaitForKeypress();
+        }
+        else
+        {
+            _logger.LogMessage("The data was not saved !", "ERROR");
+            WaitForKeypress();
+        }
+    }
+
+    private void ReturnToMainMenu()
+    {
+        Navigator.MainMenu(_logger, _factories);
+    }
+
+    private void WaitForKeypress()
+    {
+        Console.WriteLine("Press any key to return to Main Menu...");
+        Console.ReadKey(true);
+
+        ReturnToMainMenu();
+    }
+}
