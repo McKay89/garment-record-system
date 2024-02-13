7a2bbe331780165cfd5708866f4e5b06bf04459f
McKay <kg89szeged@gmail.com>
2024-02-13 19:19:54 +0100

    [ UPDATE ] sort garment UI factory added to factories

--- GarmentRecordSystem/Service/UiFactory.cs
+++ GarmentRecordSystem/Service/UiFactory.cs
@@ -18,13 +18,15 @@ public class UiFactory
         UiFactoryBase updateGarmentUiFactory = new UpdateGarmentUiFactory(_garmentBrowser);
         UiFactoryBase deleteGarmentUiFactory = new DeleteGarmentUiFactory(_garmentBrowser);
         UiFactoryBase searchGarmentUiFactory = new SearchGarmentUiFactory(_garmentBrowser);
+        UiFactoryBase sortGarmentUiFactory = new SortGarmentUiFactory(_garmentBrowser);
     
         var factories = new SortedList<int, UiFactoryBase>
         {
             { 1, addGarmentUiFactory },           // UI for creating new garment
             { 2, updateGarmentUiFactory },        // UI for updating an existing garment
             { 3, deleteGarmentUiFactory },        // UI for deleting an existing garment
-            { 4, searchGarmentUiFactory }         // UI for deleting an existing garment
+            { 4, searchGarmentUiFactory },        // UI for search a garment
+            { 5, sortGarmentUiFactory }           // UI for sorting garment list
         };
 
         return factories;
