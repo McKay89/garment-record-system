6df9140f7c01df22e25f3a630cfe9f5c097ebcc2
McKay <kg89szeged@gmail.com>
2024-02-13 20:40:16 +0100

    [ ADD ] JsonFileWriter class created

---
+++ GarmentRecordSystem/Controllers/JsonFileWriter.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using GarmentRecordSystem.Model;
+
+namespace GarmentRecordSystem.Controllers;
+
+public static class JsonFileWriter
+{
+    public static bool Write(IEnumerable<Garment>? garments)
+    {
+        try
+        {
+            var json = JsonSerializer.Serialize(garments, new JsonSerializerOptions 
+            { 
+                WriteIndented = true,
+                Converters = { new JsonStringEnumConverter() }
+            });
+            File.WriteAllText("./garments.json", json);
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+}
