82550842b2371a8c9fa4906888117fa8ab718f3e
McKay <kg89szeged@gmail.com>
2024-02-13 17:30:47 +0100

    [ ADD ] Delete UI factory created and skeleton built

---
+++ GarmentRecordSystem/Ui/DeleteGarmentUi.cs
@@ -0,0 +1,30 @@
+﻿using GarmentRecordSystem.Enums;
+using GarmentRecordSystem.Service.Logger;
+using GarmentRecordSystem.Service.Products.Browser;
+using GarmentRecordSystem.Ui.Factory;
+using GarmentRecordSystem.Utils;
+
+namespace GarmentRecordSystem.Ui;
+
+public class DeleteGarmentUi : UiBase
+{
+    private readonly IGarmentBrowser _garmentBrowser;
+    private readonly ILogger _logger = new ConsoleLogger();
+    private readonly SortedList<int, UiFactoryBase> _factories;
+    
+    public DeleteGarmentUi(string title, IGarmentBrowser garmentBrowser, SortedList<int, UiFactoryBase> factories) : base(title)
+    {
+        _garmentBrowser = garmentBrowser;
+        _factories = factories;
+    }
+
+    public override void Run()
+    {
+        Console.WriteLine("Delete Garment");
+    }
+
+    private void ReturnToMainMenu()
+    {
+        Navigator.MainMenu(_logger, _factories);
+    }
+}
---
+++ GarmentRecordSystem/Ui/Factory/DeleteGarmentUiFactory.cs
@@ -0,0 +1,20 @@
+﻿using GarmentRecordSystem.Service.Products.Browser;
+
+namespace GarmentRecordSystem.Ui.Factory;
+
+public class DeleteGarmentUiFactory : UiFactoryBase
+{
+    private readonly IGarmentBrowser _garmentBrowser;
+    
+    public DeleteGarmentUiFactory(IGarmentBrowser garmentBrowser)
+    {
+        _garmentBrowser = garmentBrowser;
+    }
+
+    public override string UiName => "Delete";
+    
+    public override UiBase Create(SortedList<int, UiFactoryBase> factories)
+    {
+        return new DeleteGarmentUi(UiName, _garmentBrowser, factories);
+    }
+}
