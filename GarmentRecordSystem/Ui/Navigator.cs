36b69c3441d1bbb86778bc33f67d0e7516a4be0f
McKay <kg89szeged@gmail.com>
2024-02-13 16:57:46 +0100

    [ ADD ] separated navigator class created. Can be used from everywhere

---
+++ GarmentRecordSystem/Ui/Navigator.cs
@@ -0,0 +1,20 @@
+﻿using GarmentRecordSystem.Service.Logger;
+using GarmentRecordSystem.Ui.Factory;
+using GarmentRecordSystem.Ui.Selector;
+
+namespace GarmentRecordSystem.Ui;
+
+public static class Navigator
+{
+    public static void MainMenu(ILogger logger, SortedList<int, UiFactoryBase> factories)
+    {
+        // Create UI Selector
+        var uiSelector = new UiSelector(logger, factories);
+        UiBase ui = uiSelector.Select();
+
+        // Run factory
+        ui.DisplayTitle();
+        ui.PrintSubmenuInfos();
+        ui.Run();
+    }
+}
