81b5abad5fbd6ad33e0e166a492988518025fc63
McKay <kg89szeged@gmail.com>
2024-02-14 18:27:54 +0100

    [ UPDATE ] exit click event implemented & search input only accept numeric characters

--- GarmentRecordSystemWPF/MainWindow.xaml.cs
+++ GarmentRecordSystemWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace GarmentRecordSystemWPF
 {
@@ -7,6 +8,25 @@ namespace GarmentRecordSystemWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!IsNumeric(e.Text))
+            {
+                
+                e.Handled = true;
+            }
+        }
+
+        private static bool IsNumeric(string text)
+        {
+            return text.All(char.IsDigit);
         }
     }
 }
