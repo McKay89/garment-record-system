868a1e0c619423f5bf4e46e0f7be063ca16e5d28
McKay <kg89szeged@gmail.com>
2024-02-14 20:19:50 +0100

    [ UPDATE ] ActivateButtons method implemented & ListView source filled with garments

--- GarmentRecordSystemWPF/MainWindow.xaml.cs
+++ GarmentRecordSystemWPF/MainWindow.xaml.cs
@@ -1,13 +1,33 @@
-﻿using System.Windows;
+﻿using GarmentRecordSystemWPF.JsonController;
+using GarmentRecordSystemWPF.Model;
+using System.Windows;
 using System.Windows.Input;
 
 namespace GarmentRecordSystemWPF
 {
     public partial class MainWindow : Window
     {
+        private List<GarmentJson>? garmentJsons;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var garmentsJson = JsonFileController.Read();
+        }
+
+        private void loadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            garmentJsons = JsonFileController.Read();
+
+            if (garmentJsons != null)
+            {
+                ActivateButtons();
+                ListGarments();
+            } else
+            {
+                // TODO: Handle error
+            }
         }
 
         private void exitBtn_Click(object sender, RoutedEventArgs e)
@@ -27,6 +47,18 @@ namespace GarmentRecordSystemWPF
         private static bool IsNumeric(string text)
         {
             return text.All(char.IsDigit);
+        }
+
+        private void ActivateButtons()
+        {
+            addBtn.IsEnabled = true;
+            searchInput.IsEnabled = true;
+            sortInput.IsEnabled = true;
+        }
+
+        private void ListGarments()
+        {            
+            garmentsList.ItemsSource = garmentJsons;
         }
     }
 }
