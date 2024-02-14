fbc5d4279e18e8019e58e0e82ec81e48f92d325a
McKay <kg89szeged@gmail.com>
2024-02-14 14:15:41 +0100

    [ UPDATE ] now check changes before exiting, can be saved

--- GarmentRecordSystem/Ui/ExitGarmentUi.cs
+++ GarmentRecordSystem/Ui/ExitGarmentUi.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using GarmentRecordSystem.Controllers;
 using GarmentRecordSystem.Enums;
 using GarmentRecordSystem.Model;
 using GarmentRecordSystem.Service.Logger;
@@ -11,17 +12,79 @@ namespace GarmentRecordSystem.Ui;
 public class ExitGarmentUi : UiBase
 {
     private readonly ILogger _logger = new ConsoleLogger();
+    private readonly IGarmentBrowser _garmentBrowser;
+    private readonly SortedList<int, UiFactoryBase> _factories;
     
-    public ExitGarmentUi(string title) : base(title)
+    public ExitGarmentUi(string title, IGarmentBrowser garmentBrowser, SortedList<int, UiFactoryBase> factories) : base(title)
     {
-
+        _garmentBrowser = garmentBrowser;
+        _factories = factories;
     }
 
     public override void Run()
     {
-        // Exit application
+        // Check if there are any changes
+        bool resultCheck = CheckChanges();
+
+        if (resultCheck)
+        {
+            // Get input
+            var input = InputValidator.ExitString(" There are unsaved changes. Do you want to save it before exit?");
+
+            switch (input)
+            {
+                case "--back":
+                    ReturnToMainMenu();
+                    break;
+                case "y" or "Y":
+                    bool resultSave = SaveChanges();
+                    if(resultSave) ExitApplication();
+                    break;
+            }
+        }
+
+        ExitApplication();
+    }
+    
+    private void ReturnToMainMenu()
+    {
+        Navigator.MainMenu(_logger, _factories);
+    }
+
+    private void ExitApplication()
+    {
         _logger.LogMessage("Application is shutting down in 2 sec...", "INFO");
         Thread.Sleep(2000);
         Environment.Exit(0);
+    }
+
+    private bool SaveChanges()
+    {
+        // Get changes
+        var changes = Changes.GetChanges();
+        
+        // Log the changes
+        _logger.LogMessage("Saving into the JSON...", "INFO");
+        Console.WriteLine($"  Changes: " + 
+                          $"{(changes.Item1 > 0 ? $"{changes.Item1} creation, " : "")}" +
+                          $"{(changes.Item2 > 0 ? $"{changes.Item2} modification, " : "")}" +
+                          $"{(changes.Item3 > 0 ? $"{changes.Item3} deletion, " : "")}" +
+                          $"{(changes.Item4 > 0 ? $"{changes.Item4} sort " : "")}");
+        
+        // Save Garments into JSON
+        var allGarments = _garmentBrowser.GetAll();
+        bool saveResult = JsonFileWriter.Write(allGarments);
+
+        return saveResult;
+    }
+
+    private bool CheckChanges()
+    {
+        if (Changes.GetChangeSum() > 0)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
--- GarmentRecordSystem/Ui/Factory/ExitGarmentUiFactory.cs
+++ GarmentRecordSystem/Ui/Factory/ExitGarmentUiFactory.cs
@@ -4,15 +4,17 @@ namespace GarmentRecordSystem.Ui.Factory;
 
 public class ExitGarmentUiFactory : UiFactoryBase
 {
-    public ExitGarmentUiFactory()
+    private readonly IGarmentBrowser _garmentBrowser;
+    
+    public ExitGarmentUiFactory(IGarmentBrowser garmentBrowser)
     {
-        
+        _garmentBrowser = garmentBrowser;
     }
 
     public override string UiName => "Exit";
     
     public override UiBase Create(SortedList<int, UiFactoryBase> factories)
     {
-        return new ExitGarmentUi(UiName);
+        return new ExitGarmentUi(UiName, _garmentBrowser, factories);
     }
 }
