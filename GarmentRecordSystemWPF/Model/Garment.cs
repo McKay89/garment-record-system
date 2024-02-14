55c2b296957ebba9a1441b37739316a523850011
McKay <kg89szeged@gmail.com>
2024-02-14 20:22:50 +0100

    [ ADD ] garment model class created

---
+++ GarmentRecordSystemWPF/Model/Garment.cs
@@ -0,0 +1,13 @@
+﻿using GarmentRecordSystemWPF.Enums;
+
+namespace GarmentRecordSystemWPF.Model
+{
+    internal class Garment
+    {
+        public int GarmentId { get; init; }
+        public string BrandName { get; init; }
+        public DateOnly PurchaseDate { get; init; }
+        public string Color { get; init; }
+        public Sizes Size { get; init; }
+    }
+}
---
+++ GarmentRecordSystemWPF/Model/GarmentJson.cs
@@ -0,0 +1,11 @@
+﻿namespace GarmentRecordSystemWPF.Model
+{
+    internal class GarmentJson
+    {
+        public int GarmentId { get; set; }
+        public string BrandName { get; set; }
+        public string PurchaseDate { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+    }
+}
