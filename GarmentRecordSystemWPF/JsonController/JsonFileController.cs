cdf836764ebee784d15480705b5a8faace43499e
McKay <kg89szeged@gmail.com>
2024-02-14 20:24:45 +0100

    [ ADD ] Json controller Read method implemented

---
+++ GarmentRecordSystemWPF/JsonController/JsonFileController.cs
@@ -0,0 +1,56 @@
+﻿using GarmentRecordSystemWPF.Model;
+using System.IO;
+using System.Text.Json;
+
+namespace GarmentRecordSystemWPF.JsonController
+{
+    internal static class JsonFileController
+    {
+        private static readonly string _filePath = "../../../garments.json";
+
+        public static List<GarmentJson>? Read()
+        {
+            try
+            {
+                var text = File.ReadAllText(_filePath);
+
+                return string.IsNullOrWhiteSpace(text)
+                    ? new List<GarmentJson>()
+                    : JsonSerializer.Deserialize<List<GarmentJson>>(text);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"File not found: {_filePath}");
+                return null;
+            }
+            catch (JsonException)
+            {
+                Console.WriteLine($"Wrong JSON format: {_filePath}");
+                return null;
+            }
+        }
+
+        public static void Write()
+        {
+            /*
+            try
+            {
+
+                var json = JsonSerializer.Serialize(garments, new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Converters = { new JsonStringEnumConverter() }
+                });
+                File.WriteAllText(_filePath, json);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+            */
+        }
+    }
+}
