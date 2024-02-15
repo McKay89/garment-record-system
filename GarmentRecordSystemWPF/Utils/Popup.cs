00b5814f0751a0779ef3333c6304a1d212afcd85
McKay <kg89szeged@gmail.com>
2024-02-15 01:16:34 +0100

    [ ADD ] confirmation and alert window implemented

---
+++ GarmentRecordSystemWPF/Utils/Popup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GarmentRecordSystemWPF.Utils
+{
+    internal static class Popup
+    {
+        public static bool Confirm(string label, string text)
+        {
+            MessageBoxResult confirmation = MessageBox.Show(text, label, MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (confirmation == MessageBoxResult.Yes)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void Alert(string label, string text)
+        {
+            MessageBox.Show(text, label, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
