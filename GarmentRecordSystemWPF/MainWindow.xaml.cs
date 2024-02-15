298bfeecdb0edb552f217b1a885a24c197b9bea6
McKay <kg89szeged@gmail.com>
2024-02-15 01:17:34 +0100

    [ UPDATE ] sorting system, item list selector and delete garment implemented

--- GarmentRecordSystemWPF/MainWindow.xaml.cs
+++ GarmentRecordSystemWPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using GarmentRecordSystemWPF.JsonController;
 using GarmentRecordSystemWPF.Model;
 using GarmentRecordSystemWPF.Service;
+using GarmentRecordSystemWPF.Utils;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -11,6 +13,8 @@ namespace GarmentRecordSystemWPF
     {
         private List<GarmentJson> garmentJson;
         private IGarmentService? garmentService;
+        private Garment? selectedGarment;
+        private int selectedGarmentIndex;
 
         public MainWindow()
         {
@@ -27,7 +31,7 @@ namespace GarmentRecordSystemWPF
             if (garmentJson != null)
             {
                 CreateGarmentService();
-                ActivateButtons();
+                SetButtonsStatus();
                 ListGarments("");
             } else
             {
@@ -56,17 +60,21 @@ namespace GarmentRecordSystemWPF
             return text.All(char.IsDigit);
         }
 
-        private void ActivateButtons()
+        private void SetButtonsStatus()
         {
-            // Enable fields and buttons
+            // Set buttons enabled & disabled
             addBtn.IsEnabled = true;
             searchInput.IsEnabled = true;
             sortInput.IsEnabled = true;
+            updateBtn.IsEnabled = false;
+            deleteBtn.IsEnabled = false;
         }
 
         private void ListGarments(string garmentId)
         {
-            if(garmentId == "")
+            garmentList.ItemsSource = new List<string>();
+
+            if (garmentId == "")
             {
                 // List all garments
                 garmentList.ItemsSource = garmentService?.GetAll();
@@ -83,12 +91,106 @@ namespace GarmentRecordSystemWPF
             garmentService = new GarmentService(garmentJson);
         }
 
+        private void DisableButtons()
+        {
+            // Disable Update & Delete buttons
+            updateBtn.IsEnabled = false;
+            deleteBtn.IsEnabled = false;
+        }
+
+        private void FocusGarment()
+        {
+            // Focus the lastly selected garment
+            garmentList.Focus();
+            garmentList.SelectedIndex = selectedGarmentIndex;
+        }
+
         private void searchInput_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
             // Check Search input when it changed
             TextBox textBox = (TextBox)sender;
             string searchText = textBox.Text;
             ListGarments(searchText);
+
+            DisableButtons();
+        }
+
+        private void sortInput_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Get ComboBox item
+            ComboBox comboBox = (ComboBox)sender;
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+
+            if (selectedItem != null)
+            {
+                string? selectedSortItem = selectedItem.Content.ToString();
+
+                // Sort by selected item
+                switch (selectedSortItem)
+                {
+                    case "Garment ID":
+                        garmentList.ItemsSource = garmentService?.SortByGarmentId();
+                        break;
+                    case "Brand Name":
+                        garmentList.ItemsSource = garmentService?.SortByBrandName();
+                        break;
+                    case "Purchase Date":
+                        garmentList.ItemsSource = garmentService?.SortByPurchaseDate();
+                        break;
+                    case "Color":
+                        garmentList.ItemsSource = garmentService?.SortByColor();
+                        break;
+                    case "Size":
+                        garmentList.ItemsSource = garmentService?.SortBySize();
+                        break;
+                }
+
+            }
+
+            DisableButtons();
+        }
+
+        private void garmentList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Get selected ListView item
+            ListView listView = (ListView)sender;
+            selectedGarmentIndex = listView.SelectedIndex;
+            selectedGarment = (Garment)listView.SelectedItem;
+
+            // Enable Update & Delete buttons
+            updateBtn.IsEnabled = true;
+            deleteBtn.IsEnabled = true;
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FocusGarment();
+
+            if (Popup.Confirm("Delete", "Are you sure you want to delete the selected item?"))
+            {
+                garmentService?.DeleteGarment(selectedGarment?.GarmentId);
+                ListGarments("");
+            }
+        }
+
+        private void updateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FocusGarment();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DisableButtons();
+        }
+
+        private void searchInput_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            DisableButtons();
+        }
+
+        private void sortInput_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            DisableButtons();
         }
     }
 }
